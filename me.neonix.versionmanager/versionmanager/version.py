ce04eb19bad5e296b9288033fb4d5207dfa0d33f
Acherons_ <kamron.cole13@gmail.com>
2022-06-10 00:01:07 -0400

    add comments

--- me.neonix.versionmanager/versionmanager/version.py
+++ me.neonix.versionmanager/versionmanager/version.py
@@ -10,15 +10,18 @@ class Version:
         self.__version_list = list(version.split(separator))
 
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__version_str
 
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Version[{self.__version_str}, {self.__version_list}]'
 
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
+        '''
+        Compare each version category to tell if local version is less than, equal to, or greater than the latest GitHub release
+        '''
         for i in range(len(self.__version_list)):
             version_number = int(self.__version_list[i])
             latest_version_number = int(other.__version_list[i])
@@ -29,7 +32,7 @@ class Version:
         return False
 
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if type(other) != Version:
             return False
 
--- me.neonix.versionmanager/versionmanager/versionmanager.py
+++ me.neonix.versionmanager/versionmanager/versionmanager.py
@@ -1,19 +1,34 @@
+'''
+A GitHub Project Version Manager that polls latest version data from GitHub repo release tag
+
+author: Kamron Cole (kjc8084@rit.edu)
+'''
 from enum import Enum
 from event import Event
 from local import Local
 from remote import Remote
 
-class Result(Enum):
+class Status(Enum):
     OUTDATED = -1,
     CURRENT = 0,
     DEV = 1
 
 
 class VersionManager:
+    '''
+    The main class to compare a local project to the latest Github release and perform actions based on the result
+    '''
     __slots__ = ['__local', '__remote', '__author', '__projectName', '__separator', 'outdated_event', 'current_event', 'dev_event']
 
 
     def __init__(self, author: str, projectName: str, version: str, separator='.'):
+        '''
+        Instantiate the VersionManager for a project
+        author: the owner of the project's repo
+        projectName: the name of the project's repo
+        version: the semantic version of the project as a string
+        separator: the separator used to separate the version categories
+        '''
         self.__author = author
         self.__projectName = projectName
         self.__separator = separator
@@ -26,7 +41,11 @@ class VersionManager:
         self.dev_event = Event()
 
 
-    def check_status(self):
+    def check_status(self) -> Status:
+        '''
+        Compare local project version to the latest GitHub release version. Trigger events based on comparison result
+        returns: Result of version comparison in form of values from the Status Enum
+        '''
         if self.__remote is None:
             self.__remote = Remote(self.__author, self.__projectName, self.__separator)
         else:
@@ -34,16 +53,20 @@ class VersionManager:
 
         if self.__local.verison() == self.__remote.verison():
             self.current_event()
-            return Result.CURRENT
+            return Status.CURRENT
         elif self.__local.verison() < self.__remote.verison():
             self.outdated_event()
-            return Result.OUTDATED
+            return Status.OUTDATED
         else:
             self.dev_event()
-            return Result.DEV
+            return Status.DEV
 
 
-    def get_data(self):
+    def get_data(self) -> dict:
+        '''
+        Get various other information about the latest GitHub release from the parsed JSON
+        returns: A dictionary representation of the JSON
+        '''
         if self.__remote is None:
             self.__remote = Remote(self.__author, self.__projectName, self.__separator)
         return self.__remote.get_data()
